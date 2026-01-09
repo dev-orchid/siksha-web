124f2463b95df28994562eda16022ad59a1aaf3d
KEDAR KUMAR <kedarkumar@KEDARs-MacBook-Pro.local>
2026-01-09 15:22:08 +0530

    first commit 1.0.0

--- src/app/sms/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { SessionProvider } from '@/components/providers/SessionProvider'
-
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <SessionProvider>{children}</SessionProvider>
-}
